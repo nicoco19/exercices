abf951bc575e3fb612665174dd3d80de4e9b88c0
nicolasventurini <nicolas.venturini@student.vinci.be>
2024-09-24 14:54:28 +0200

    new:ex2.1-destructuring

--- 2.1/src/ClickCounter.jsx
+++ 2.1/src/ClickCounter.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react';
 
 function ClickCounter() {
   const [count, setCount] = useState(0)
+  const [title,setTitel] = useState("")
   console.log(`le compteur est : ${count}`);
+  if(count === 10 && title === ""){
+    setTitel("You are a master in the art of clicking !")
+  }
   return (
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-
-
-      </button>
+      <>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>{title}</p>
+      </>
 
   );
 }
