90074debfeb0c1261d48bfe54f20d186da23cc80
nicolasventurini <nicolas.venturini@student.vinci.be>
2024-09-24 15:21:03 +0200

    new:ex2.1-stored

--- 2.1/src/ClickCounter.jsx
+++ 2.1/src/ClickCounter.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 
 function ClickCounter() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem("count");
+    return savedCount ? JSON.parse(savedCount) : 0;
+  });
   const [title,setTitel] = useState("")
+  const [message , setMessage] = useState(`count is ${count}`)
   console.log(`le compteur est : ${count}`);
   if(count === 10 && title === ""){
     setTitel("You are a master in the art of clicking !")
   }
   return (
       <>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() =>{setCount((count) => count + 1); localStorage.setItem("count", JSON.stringify(count+1))}} onMouseEnter={() => setMessage("Please click on me now !")} onMouseLeave={() => setMessage(`count is ${count}`)}>
+          {message}
         </button>
         <p>{title}</p>
       </>
